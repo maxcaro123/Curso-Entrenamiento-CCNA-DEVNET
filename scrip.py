36d11d85fd8d002385417de8487097d18471b5c8
maxcaro123 <mw.aguilera@duocuc.cl>
2024-06-29 23:32:44 +0000

    Initial commit: Add script.py

---
+++ scrip.py
@@ -0,0 +1,13 @@
+def main():
+    nombre = "Max"
+    apellido = "Aguilera"
+    edad = "26"
+    sede = "Duoc UC Maipu"
+
+    print(f"Nombre: {nombre}")
+    print(f"Apellido: {apellido}")
+    print(f"Edad: {edad}")
+    print(f"Sede: {sede}")
+
+if __name__ == "__main__":
+    main()
